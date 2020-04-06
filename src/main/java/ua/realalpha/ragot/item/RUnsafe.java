48241f1aa85d7e35f4e9b1b77df1bbe1dd0c3986
RealAlphaDEV <realalpha.ua@gmail.com>
2020-04-06 21:14:46 +0200

    fix untraceable method

--- src/main/java/ua/realalpha/ragot/item/RUnsafe.java
+++ src/main/java/ua/realalpha/ragot/item/RUnsafe.java
@@ -8,65 +8,65 @@ public class RUnsafe {
     }
 
     public String getString(String tag){
-        return new RNBTItem(this.RItemBuilder.toItemStack()).getString(tag);
+        return new RNBTItem(this.RItemBuilder).getString(tag);
     }
 
     public RUnsafe setString(String tag, String value){
-        this.RItemBuilder = new RItemBuilder(new RNBTItem(this.RItemBuilder.toItemStack()).setString(tag, value).build());
+        this.RItemBuilder = new RItemBuilder(new RNBTItem(this.RItemBuilder).setString(tag, value).build());
         return this;
     }
 
     public RUnsafe setByte(String tag, byte b){
-        this.RItemBuilder = new RItemBuilder(new RNBTItem(this.RItemBuilder.toItemStack()).setByte(tag, b).build());
+        this.RItemBuilder = new RItemBuilder(new RNBTItem(this.RItemBuilder).setByte(tag, b).build());
         return this;
     }
 
     public byte getByte(String tag){
-        return new RNBTItem(this.RItemBuilder.toItemStack()).getByte(tag);
+        return new RNBTItem(this.RItemBuilder).getByte(tag);
     }
 
     public RUnsafe setShort(String tag, short b){
-        this.RItemBuilder = new RItemBuilder(new RNBTItem(this.RItemBuilder.toItemStack()).setShort(tag, b).build());
+        this.RItemBuilder = new RItemBuilder(new RNBTItem(this.RItemBuilder).setShort(tag, b).build());
         return this;
     }
 
     public short getShort(String tag){
-        return new RNBTItem(this.RItemBuilder.toItemStack()).getShort(tag);
+        return new RNBTItem(this.RItemBuilder).getShort(tag);
     }
 
     public RUnsafe setInt(String tag, int b){
-        this.RItemBuilder = new RItemBuilder(new RNBTItem(this.RItemBuilder.toItemStack()).setInt(tag, b).build());
+        this.RItemBuilder = new RItemBuilder(new RNBTItem(this.RItemBuilder).setInt(tag, b).build());
         return this;
     }
 
     public int getInt(String tag){
-        return new RNBTItem(this.RItemBuilder.toItemStack()).getInt(tag);
+        return new RNBTItem(this.RItemBuilder).getInt(tag);
     }
 
     public RUnsafe setByteArray(String tag, byte[] b){
-        this.RItemBuilder = new RItemBuilder(new RNBTItem(this.RItemBuilder.toItemStack()).setByteArray(tag, b).build());
+        this.RItemBuilder = new RItemBuilder(new RNBTItem(this.RItemBuilder).setByteArray(tag, b).build());
         return this;
     }
 
     public byte[] getByteArray(String tag){
-        return new RNBTItem(this.RItemBuilder.toItemStack()).getByteArray(tag);
+        return new RNBTItem(this.RItemBuilder).getByteArray(tag);
     }
 
     public RUnsafe setIntArray(String tag, int[] b){
-        this.RItemBuilder = new RItemBuilder(new RNBTItem(this.RItemBuilder.toItemStack()).setIntArray(tag, b).build());
+        this.RItemBuilder = new RItemBuilder(new RNBTItem(this.RItemBuilder).setIntArray(tag, b).build());
         return this;
     }
 
     public int[] getIntArray(String tag){
-        return new RNBTItem(this.RItemBuilder.toItemStack()).getIntArray(tag);
+        return new RNBTItem(this.RItemBuilder).getIntArray(tag);
     }
 
     public boolean containsTag(String s){
-        return new RNBTItem(this.RItemBuilder.toItemStack()).containsTag(s);
+        return new RNBTItem(this.RItemBuilder).containsTag(s);
     }
 
     public RUnsafe remove(String tag){
-        this.RItemBuilder = new RItemBuilder(new RNBTItem(this.RItemBuilder.toItemStack()).removeTag(tag).build());
+        this.RItemBuilder = new RItemBuilder(new RNBTItem(this.RItemBuilder).removeTag(tag).build());
         return this;
     }
 
