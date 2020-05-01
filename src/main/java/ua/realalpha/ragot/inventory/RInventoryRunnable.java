66c5db9b052ebb1649d29693cd43084c785f6fcc
RealAlphaDEV <realalpha.ua@gmail.com>
2020-05-01 21:34:55 +0200

    add rinventoryrunnable

---
+++ src/main/java/ua/realalpha/ragot/inventory/RInventoryRunnable.java
@@ -0,0 +1,23 @@
+package ua.realalpha.ragot.inventory;
+
+import java.util.UUID;
+
+public class RInventoryRunnable {
+
+    private final Runnable runnable;
+    private final int delay;
+    private final UUID uuid;
+
+    public RInventoryRunnable(Runnable runnable, int delay) {
+        this.runnable = runnable;
+        this.delay = delay;
+        this.uuid = UUID.randomUUID();
+    }
+
+
+    public UUID getUuid() { return uuid; }
+    public int getDelay() { return delay; }
+    public Runnable getRunnable() {
+        return runnable;
+    }
+}
