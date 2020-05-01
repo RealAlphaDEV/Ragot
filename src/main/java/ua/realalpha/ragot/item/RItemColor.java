452426cf6c7e01a4a8c647e0685430989ec75c8c
RealAlphaDEV <realalpha.ua@gmail.com>
2020-05-01 20:22:41 +0200

    add rconsumer & ritemcolor

---
+++ src/main/java/ua/realalpha/ragot/item/RItemColor.java
@@ -0,0 +1,30 @@
+package ua.realalpha.ragot.item;
+
+public enum RItemColor {
+
+    WHITE(0),
+    ORANGE(1),
+    MAGENTA(2),
+    LIGHT_BLUE(3),
+    YELLOW(4),
+    LIME(5),
+    PINK(6),
+    GRAY(7),
+    SILVER(8),
+    CYAN(9),
+    PURPLE(10),
+    BLUE(11),
+    BROWN(12),
+    GREEN(13),
+    RED(14),
+    BLACK(15);
+
+    private final int data;
+    RItemColor(int data) {
+        this.data = data;
+    }
+
+    public short getData() {
+        return (short) data;
+    }
+}
