20ebc8f9ef72b3a657d9774b1761637ba0fcfc60
RealAlphaDEV <realalpha.ua@gmail.com>
2020-05-03 20:58:40 +0200

    update ritemdata

--- src/main/java/ua/realalpha/ragot/item/RItemData.java
+++ src/main/java/ua/realalpha/ragot/item/RItemData.java
@@ -1,21 +1,23 @@
 package ua.realalpha.ragot.item;
 
+import org.bukkit.event.Event;
+
 import java.io.Serializable;
 import java.util.HashMap;
 
-public final class RItemEvent implements Serializable {
+public final class RItemData implements Serializable {
 
-    private final HashMap<Class<?>, RConsumer<?>> rConsumerHashMap;
+    private final HashMap<Class<? extends Event>, RConsumer<? extends Event>> rConsumerHashMap;
 
-    public RItemEvent() {
+    public RItemData() {
         this.rConsumerHashMap = new HashMap<>();
     }
 
-    public void put(Class<?> aClass, RConsumer<?> rConsumer){
+    public void put(Class<? extends Event> aClass, RConsumer<? extends Event> rConsumer){
         this.rConsumerHashMap.put(aClass, rConsumer);
     }
 
-    public RConsumer<?> get(Class<?> aClass){
+    public RConsumer<? extends Event> get(Class<? extends Event> aClass){
         return this.rConsumerHashMap.getOrDefault(aClass, o -> {});
     }
 
