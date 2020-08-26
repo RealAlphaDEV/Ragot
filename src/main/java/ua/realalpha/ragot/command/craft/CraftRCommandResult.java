0410429a843b43c3692d660010580843e32488eb
RealAlphaUA <realalpha.ua@gmail.com>
2020-08-26 23:58:30 +0200

    update CraftRCommandResult 🧱

--- src/main/java/ua/realalpha/ragot/command/craft/CraftRCommandResult.java
+++ src/main/java/ua/realalpha/ragot/command/craft/CraftRCommandResult.java
@@ -1,44 +1,49 @@
 package ua.realalpha.ragot.command.craft;
 
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-import ua.realalpha.ragot.command.entity.RArgument;
-import ua.realalpha.ragot.command.entity.CommandResult;
-import ua.realalpha.ragot.command.entity.RGroupCommand;
+import ua.realalpha.ragot.command.entity.RCommand;
+import ua.realalpha.ragot.command.entity.RCommandResult;
+import ua.realalpha.ragot.command.entity.RTweakArgument;
 
-import java.util.Optional;
+import java.util.Arrays;
+import java.util.List;
 
-public class CraftCommandResult implements CommandResult {
+public final class CraftRCommandResult implements RCommandResult {
 
     private final CommandSender commandSender;
+    private final CraftRTweakArgument craftRArgument;
+    private final String[] args;
+    private List<RCommand> rCommands;
 
-    public CraftCommandResult(CommandSender commandSender) {
+    public CraftRCommandResult(CommandSender commandSender, String[] args, int index, List<RCommand> rCommands) {
         this.commandSender = commandSender;
+        this.args = tweakArguments(Arrays.asList(args), index);
+        this.craftRArgument = new CraftRTweakArgument(this.args);
+        this.rCommands = rCommands;
+    }
+
+
+    @Override
+    public <T extends CommandSender> T asSender(Class<T> tClass) {
+        return (T) commandSender;
     }
 
     @Override
-    public CommandSender asConsole() {
-        return commandSender;
-    }
-
-    @Override
-    public Optional<Player> asPlayer() {
-        if (commandSender instanceof Player) return Optional.of((Player) commandSender);
-        return Optional.empty();
-    }
-
-    @Override
-    public RArgument getRArgument() {
-        return null;
+    public RTweakArgument getRTweakArgument() {
+        return craftRArgument;
     }
 
     @Override
     public String[] getArgs() {
-        return new String[0];
+        return args;
     }
 
     @Override
-    public RGroupCommand getRGroupCommand() {
-        return null;
+    public List<RCommand> getTopRsCommand() {
+        return rCommands;
+    }
+
+    private String[] tweakArguments(List<String> args, int index){
+        return args.subList(index, args.size()).toArray(new String[]{});
     }
 }
