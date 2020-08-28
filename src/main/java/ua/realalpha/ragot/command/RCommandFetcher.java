9663a5fa0b41ca9ec127fa1e36d1cc13d1034ac1
RealAlphaUA <realalpha.ua@gmail.com>
2020-08-28 22:39:06 +0200

    update RCommandFetcher 🔩

--- src/main/java/ua/realalpha/ragot/command/RCommandFetcher.java
+++ src/main/java/ua/realalpha/ragot/command/RCommandFetcher.java
@@ -1,64 +1,65 @@
 package ua.realalpha.ragot.command;
 
-import ua.realalpha.ragot.command.data.RCommandData;
+
+import ua.realalpha.ragot.command.handler.RCommandHandler;
+import ua.realalpha.ragot.command.handler.RCommandRegister;
 
 import java.util.*;
-import java.util.stream.Collectors;
-
 
 public class RCommandFetcher {
 
-    private final RCommandData rCommandData;
-
-    public RCommandFetcher(RCommandData rCommandData) {
-        this.rCommandData = rCommandData;
+    private final Set<RCommandHandler> rCommandHandlers;
+    private final RCommandRegister rCommandRegister;
+    public RCommandFetcher(RCommandRegister rCommandRegister, Set<RCommandHandler> rCommandHandlers) {
+        this.rCommandHandlers = rCommandHandlers;
+        this.rCommandRegister = rCommandRegister;
     }
 
-    public RCommandData getRCommand(List<RCommandData> list, List<String> args) {
-        final Map<RCommandData, Integer> rCommandDataIntegerMap = new HashMap<>();
-        list.forEach(rCommandData -> rCommandDataIntegerMap.put(rCommandData, getMatchLevel(rCommandData, args)));
+    public Set<String[]> getCommand(RCommandHandler rCommandHandler) {
+        final Set<String[]> listMatch = new HashSet<>();
+        final Set<String[]> subCommands = new HashSet<>();
+        final List<String> command = new ArrayList<>(Arrays.asList(rCommandHandler.aliases()));
+        command.add((rCommandHandler.command().length == 1 ? rCommandHandler.command()[0] : rCommandHandler.command()[rCommandHandler.command().length - 1]));
 
-        LinkedHashMap<RCommandData, Integer> collect = rCommandDataIntegerMap.entrySet()
-                .stream()
-                .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
-                .collect(
-                        Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e2,
-                                LinkedHashMap::new));
-        Iterator<Map.Entry<RCommandData, Integer>> iterator = collect.entrySet().iterator();
-        if (iterator.hasNext()){
-            final Map.Entry<RCommandData, Integer> next = iterator.next();
-            final List<String> command = new ArrayList<>(next.getKey().getCommand());
-            command.remove(0);
-            final List<String> commonElement = findCommonElement(command, args);
-            if (!next.getKey().getArgs().isEmpty()){
-                if ((commonElement.size() + next.getKey().getArgs().size()) == args.size()) return next.getKey();
-            } else {
-                if (!commonElement.isEmpty()) return next.getKey();
+        if (rCommandHandler.command().length == 1) {
+            for (String s : command) {
+                listMatch.add(new String[]{s});
             }
+            return listMatch;
         }
 
-        return rCommandData;
-    }
-
-
-    private int getMatchLevel(RCommandData rCommandData, List<String> args) {
-        final List<String> command = new ArrayList<>(rCommandData.getCommand());
-        command.remove(0);
-        List<String> commonElement = findCommonElement(command, args);
-        if (!commonElement.isEmpty()) {
-            int argsRequired = commonElement.size() + rCommandData.getArgs().size();
-            if (!rCommandData.getArgs().isEmpty()) {
-                if (argsRequired == args.size()) return argsRequired;
-                return commonElement.size();
-            }
-            return commonElement.size();
+        final List<String> subCommand = Arrays.asList(rCommandHandler.command()).subList(1, rCommandHandler.command().length - 1);
+        final Optional<RCommandHandler> optionalRCommandHandler = this.getByListCommand(subCommand);
+        if (optionalRCommandHandler.isPresent()) {
+            subCommands.addAll(new RCommandFetcher(rCommandRegister, rCommandHandlers).getCommand(optionalRCommandHandler.get()));
+        } else if (subCommand.size() > 1) {
+            subCommands.addAll(new RCommandFetcher(rCommandRegister, rCommandHandlers).getCommand(new RMockCommandHandler(subCommand.toArray(new String[0]))));
         }
-        return 0;
+
+        for (String[] list : subCommands) {
+            for (String alias : command) {
+                List<String> strings = new ArrayList<>(Arrays.asList(list));
+                strings.add(alias);
+                listMatch.add(strings.toArray(new String[0]));
+            }
+        }
+        return listMatch;
     }
 
-    private List<String> findCommonElement(List<String> a, List<String> b){
-        List<String> list = new ArrayList<>(a);
-        list.retainAll(b);
-        return list;
+    private Optional<RCommandHandler> getByListCommand(List<String> command){
+        if (command.size() == 0) return Optional.of(new RMockCommandHandler(new String[]{rCommandRegister.command()}, rCommandRegister.aliases(), "", rCommandRegister.description(), rCommandRegister.usage()));
+        List<String> strings = new ArrayList<>();
+        strings.add(rCommandRegister.command());
+        strings.addAll(command);
+        for (RCommandHandler rCommandHandler : this.rCommandHandlers) {
+            if (Arrays.asList(rCommandHandler.command()).toString().equalsIgnoreCase(strings.toString())) return Optional.of(rCommandHandler);
+        }
+        return Optional.empty();
     }
+
+
+
+
+
+
 }
